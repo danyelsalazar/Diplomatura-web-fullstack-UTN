912c4af157242f36ca37377d72842890040fb4f6
danyel salazar <danygasia2@gmail.com>
2026-02-05 13:08:05 -0300

    add: api de contactos para renderizar en el componente aside de la app

--- clase-25-inicio-tp/src/components/Aside.jsx
+++ clase-25-inicio-tp/src/components/Aside.jsx
@@ -1,9 +1,32 @@
 import { users } from "../services/api"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 const Aside= ()=>{
 
     const [search, setSearch] = useState("")
+    const [contactos, setContactos] = useState([])
+
+    const fecchinContactos = async ()=>{
+        try {
+            const response = await fetch("https://dummyjson.com/users") 
+            
+            if(!response.ok){
+                throw new Error("No se pudo cargar la api")
+            }
+
+            const data = await response.json()
+            // console.log(data.users);
+            setContactos(data.users)
+
+        } catch (e) {
+            console.error(e.message);
+            
+        }
+    } 
+
+    useEffect(()=>{
+        fecchinContactos()
+    },[])
 
     const handleChange= (e)=>{
         // console.log("modificando input");
@@ -11,19 +34,25 @@ const Aside= ()=>{
         
     }
 
-    const filterUsers = users.filter((user)=> user.name.toLowerCase().includes(search.toLowerCase()))
+    const filterContacts = contactos.filter((contacto)=> contacto.firstName.toLowerCase().includes(search.toLowerCase()) || contacto.lastName.toLowerCase().includes(search.toLowerCase()))
     // console.log(filterUsers);
     
 
     return(
         <aside>
             <h1>Chat UTN</h1>
-            <input type="search" placeholder="Buscar contactos..." onChange={handleChange}/>
+            <input type="search" placeholder="Buscar contactos..." onChange={handleChange} className="search"/>
             <ul>
             {
-                filterUsers.map((user)=>(
-                    <li>{user.name}
-                    <small>{user.status}</small>
+                filterContacts.map((contact)=>(
+                    <li>
+                        <div className="container-img-chat">
+                            <img src={contact.image} alt="" />
+                        </div>
+                        <div className="container-descripcion-chat">
+                            <h3>{contact.firstName} {contact.lastName}</h3>
+                            <small>{contact.address.state}</small>
+                        </div>
                     </li>
                 ))
             }
